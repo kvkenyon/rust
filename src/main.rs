70175bb5cff0cf6fa52a02089facafe970e32f86
Kevin <kvkenyon@gmail.com>
2025-07-18 16:30:15 -0500

    error handling practice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{self, ErrorKind, Read};
+
 mod hashy;
 
 #[derive(Debug)]
@@ -69,9 +72,98 @@ mod vectors {
         let hello = String::from("Здравствуйте");
         println!("len {} = {}", hello, hello.len());
     }
+
+    pub fn median(v: &mut Vec<i32>) -> f64 {
+        v.sort();
+        if v.len() % 2 == 0 {
+            let mid = v[v.len() / 2];
+            let mid2 = v[(v.len() / 2) - 1];
+            return ((mid + mid2) / 2).into();
+        }
+
+        v[v.len() / 2].into()
+    }
 }
 
 fn main() {
     vectors::test();
     hashy::hashy::test();
+
+    let mut v = vec![3, 2, 1];
+
+    let med = vectors::median(&mut v);
+
+    println!("median={med}");
+
+    let mut v = vec![1, 2, 3, 4];
+    let med = vectors::median(&mut v);
+    println!("expected=2.5 actual={med}");
+
+    test(&mut v);
+    let username = read_username_from_file_shorter(&String::from("username.txt"));
+    match username {
+        Ok(un) => println!("Username: {un}"),
+        Err(e) => panic!("There was an error reaading the username: {e:?}"),
+    }
+}
+
+fn test(v: &mut Vec<i32>) {
+    let n = v.len();
+    for (i, t) in v.iter().enumerate().rev() {
+        println!("i={i} t={t}");
+    }
+    println!("n={n}");
+}
+
+fn error_handling_examples() {
+    let file = File::open("hello.txt");
+
+    let file = match file {
+        Ok(f) => f,
+
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(f) => f,
+                Err(e) => panic!("Error creating file: {e:?}"),
+            },
+            _ => {
+                panic!("Problem opening file: {e:?}");
+            }
+        },
+    };
+}
+
+fn error_handling_examples_clean() {
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem opening file: {error:?}");
+            })
+        } else {
+            panic!("Problem opening file: {error:?}");
+        }
+    });
+}
+
+fn read_username_from_file(filename: &String) -> Result<String, io::Error> {
+    let username_file_result = File::open(filename);
+
+    let mut username_file = match username_file_result {
+        Ok(f) => f,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_shorter(filename: &String) -> Result<String, io::Error> {
+    let mut username_file = File::open(filename)?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
 }
