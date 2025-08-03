2d0b15652610b57beafefc4fc80f1fb5dbce8f7b
Kevin <kvkenyon@gmail.com>
2025-08-03 14:23:54 -0500

    reference cycles with refcell<rc<list>>

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use rust_learning::error_handling;
 use rust_learning::generics;
 use rust_learning::iterators;
 use rust_learning::lifetimes;
+use rust_learning::reference_cycles;
 use rust_learning::smart_pointers;
 use rust_learning::traits;
 use rust_learning::vectors;
@@ -18,4 +19,5 @@ fn main() {
     closures::test();
     iterators::test();
     smart_pointers::test();
+    reference_cycles::test();
 }
---
+++ src/reference_cycles.rs
@@ -0,0 +1,46 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, rest) => Some(rest),
+            Nil => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+use crate::reference_cycles::List::{Cons, Nil};
+
+pub fn test() {
+    println!("[reference_cycles] Start...");
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+    println!("a ref count after create: {}", Rc::strong_count(&a));
+    println!("a next item: {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+    println!("a ref count after create b: {}", Rc::strong_count(&a));
+    println!("b initial ref count: {}", Rc::strong_count(&b));
+    println!("b next item: {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b ref count after changing a: {}", Rc::strong_count(&b));
+    println!("a ref count after changing b: {}", Rc::strong_count(&a));
+
+    println!("[reference_cycles] End...");
+}
